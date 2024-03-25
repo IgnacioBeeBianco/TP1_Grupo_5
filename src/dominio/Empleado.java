cbdeaf89f0719bb8549b487d46b46553f4abbccc
IgnacioBeeBianco <105438704+IgnacioBeeBianco@users.noreply.github.com>
2024-03-25 12:15:20 -0300

    consigna del ejercicio 1

--- src/dominio/Empleado.java
+++ src/dominio/Empleado.java
@@ -1,5 +1,11 @@
 package dominio;
 
+/*
+1. Crear una clase Empleado con las siguientes propiedades:
+		int id;
+		String nombre;
+		int edad;
+*/
 public class Empleado {
 	int id;
 	String nombre;
