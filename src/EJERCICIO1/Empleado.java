e73fc9eeaa7bae9f064ff2262a3365c84bf7cb5e
purplesunk <84647408+purplesunk@users.noreply.github.com>
2024-03-27 10:50:09 -0300

    Agregar setters y getters

--- src/EJERCICIO1/Empleado.java
+++ src/EJERCICIO1/Empleado.java
@@ -14,6 +14,22 @@ public class Empleado {
 	private int id;
 	private String nombre;
 	private int edad;
+	
+	public int getId() {
+		return id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 }
 
 
